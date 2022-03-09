cbcdbcd6ba7fce78e809fa50ff6348022068036f
Tammam Khan <tammam.a.khan@gmail.com>
2022-03-08 19:57:47 -0800

    added storing tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,9 +33,6 @@ var taskFormHandler = function (event) {
       status: "to do",
     };
 
-    taskDataObj.id = taskIdCounter;
-    tasks.push(taskDataObj);
-
     createTaskEl(taskDataObj);
   }
 };
@@ -61,6 +58,11 @@ var createTaskEl = function (taskDataObj) {
   listItemEl.appendChild(taskActionsEl);
 
   tasksToDoEl.appendChild(listItemEl);
+
+  taskDataObj.id = taskIdCounter;
+  tasks.push(taskDataObj);
+
+  saveTasks();
 
   // increase task counter for next unique id
   taskIdCounter++;
@@ -126,6 +128,8 @@ var deleteTask = function (taskId) {
 
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+
+  saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -161,6 +165,8 @@ var completeEditTask = function (taskName, taskType, taskId) {
       tasks[i].type = taskType;
     }
   }
+
+  saveTasks();
 
   alert("Task Updated!");
 
@@ -210,6 +216,12 @@ var taskStatusChangeHandler = function (event) {
       tasks[i].status = statusValue;
     }
   }
+
+  saveTasks();
+};
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 formEl.addEventListener("submit", taskFormHandler);
